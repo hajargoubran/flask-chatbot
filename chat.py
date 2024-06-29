ded1bf92d2ef45665e86d8b41f5924675796e21a
hajargoubran <hagergobrann@gmail.com>
2024-06-29 12:19:36 +0200

    commit

--- chat.py
+++ chat.py
@@ -2,8 +2,8 @@ from flask import Flask, request, jsonify
 from chatterbot import ChatBot
 import pandas as pd
 import random
-import smtplib
-from email.mime.text import MIMEText
+""" import smtplib
+from email.mime.text import MIMEText """
 
 app = Flask(__name__)
 bot = ChatBot("TravelBot")
@@ -58,13 +58,13 @@ def handle_complaint():
     reference_number =  random.choice(numbers)
     print(f"Complaint received. Your reference number is Comp-{reference_number}.")
     # Send complaint data (name, email, complaint) with reference number to back office (implement logic here)
-    subject = f"Comp-{reference_number}"
+    """ subject = f"Comp-{reference_number}"
     body = complaint
     sender = "T.chatbot@gmail.com"
     recipients = ["travelmania.backoffice@gmail.com" ]
     password = "Travel@161446"
-    send_email(subject, body, sender, recipients, password)
-    return f"Thank you, {name}. We'll look into your complaint (Comp-{reference_number}) shortly."
+    send_email(subject, body, sender, recipients, password)"""
+    return f"Thank you, {name}. We'll look into your complaint (Comp-{reference_number}) shortly." 
 
 
 def handle_technical_issue():
@@ -77,12 +77,12 @@ def handle_technical_issue():
     reference_number = random.choice(numbers1)
     print(f"Technical issue received. Your reference number is Tech-{reference_number}.")
     # Send technical issue data (name, email, description) with reference number to back office
-    subject = f"Tech-{reference_number}"
+    """  subject = f"Tech-{reference_number}"
     body = description
     sender = "T.chatbot@gmail.com"
     recipients = ["travelmania.backoffice@gmail.com" ]
     password = "Travel@161446"
-    send_email(subject, body, sender, recipients, password)
+    send_email(subject, body, sender, recipients, password) """
     return f"Thank you, {name}. We'll investigate the issue (Tech-{reference_number}) shortly."
 
 
